71d367690ee7ceb3b132ca577364551fccf58544
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-31 18:06:53 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar"> 
-      <NavLink to="/">Home<
+      <NavLink to="/">Home</navLink>
       
     </div>
   );
